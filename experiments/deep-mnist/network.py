4314d25e30e7290a1843a23bbc963aecc5279ee5
babaktr <babak@toghiani.se>
2017-01-26 21:14:38 +0100

    Rename network components for easier reading of the graph

--- experiments/deep-mnist/network.py
+++ experiments/deep-mnist/network.py
@@ -45,49 +45,53 @@ class ConvolutionalNeuralNetwork(object):
             self.y_ = tf.placeholder(tf.float32, shape=[None, 10], name='desired-output')
 
             # Convolutional layer 1 weights and bias
-            W_conv1 = self.weight_variable([5, 5, 1, 32], name='w-conv1')
-            b_conv1 = self.bias_variable([32], name='b-conv1')
+            with tf.name_scope('conv1') as scope
+                W_conv1 = self.weight_variable([5, 5, 1, 32], name='w-conv1')
+                b_conv1 = self.bias_variable([32], name='b-conv1')
 
-            # First conv layer output
-            with tf.name_scope('conv1') as scope:
-                h_conv1 = tf.nn.relu(self.conv2d(x_img, W_conv1) + b_conv1)
+                # First conv layer output
+                with tf.name_scope('conv1-out') as scope:
+                    h_conv1 = tf.nn.relu(self.conv2d(x_img, W_conv1) + b_conv1)
 
             # First layer pooling
             with tf.name_scope('max-pool1') as scope:
                 h_pool1 = self.max_pool_2x2(h_conv1)
 
             # Second layer conv weights and biases
-            W_conv2 = self.weight_variable([5, 5, 32, 64], name='w-conv2')
-            b_conv2 = self.bias_variable([64], name='b-conv2')
+            with tf.name_scope('conv2') as scope
+                W_conv2 = self.weight_variable([5, 5, 32, 64], name='w-conv2')
+                b_conv2 = self.bias_variable([64], name='b-conv2')
 
-            # Second layer conv output
-            with tf.name_scope('conv2') as scope:
-                h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
+                # Second layer conv output
+                with tf.name_scope('conv2-out') as scope:
+                    h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
 
             # Second layer pooling
             with tf.name_scope('max-pool2') as scope:
                 h_pool2 = self.max_pool_2x2(h_conv2)
 
             # Fully connected layer with weights and biases
-            W_fc1 = self.weight_variable([7 * 7 * 64, 1024], name='w-fc')
-            b_fc1 = self.bias_variable([1024], name='b-fc')
-
-            # Fully connected layer output
             with tf.name_scope('fully-connected') as scope:
-                h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-                h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+                W_fc1 = self.weight_variable([7 * 7 * 64, 1024], name='w-fc')
+                b_fc1 = self.bias_variable([1024], name='b-fc')
+
+                # Fully connected layer output
+                with tf.name_scope('fully-connected-out') as scope:
+                    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+                    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
             # Drop out to reduce overfitting
             self.keep_prob = tf.placeholder(tf.float32, name='keep-prob')
             h_fc1_drop = tf.nn.dropout(h_fc1, self.keep_prob)
 
             # Output layer weights and biases
-            W_fc2 = self.weight_variable([1024, 10], name='w-out')
-            b_fc2 = self.bias_variable([10], name='b-out')
-
-            # Output
             with tf.name_scope('output') as scope:
-                self.y = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+                W_fc2 = self.weight_variable([1024, 10], name='w-out')
+                b_fc2 = self.bias_variable([10], name='b-out')
+
+                # Output
+                with tf.name_scope('classification') as scope:
+                    self.y = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
             # Objective/Error function - Cross Entropy
             with tf.name_scope('loss') as scope:
