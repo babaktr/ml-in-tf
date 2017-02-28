210354abe46e30cfde703977cac8b6f8146e0571
Babak Toghiani-Rizi <babak@spire.se>
2017-02-28 19:48:57 +0100

    Clean up code

--- experiments/rnn-mnist/network.py
+++ experiments/rnn-mnist/network.py
@@ -8,11 +8,13 @@ class RecurrentNeuralNetwork(object):
             # Set random seed
             tf.set_random_seed(random_seed)
 
-            # Input with shape [?, 28, 28]
+            # Input with shape [?, sequence_length * input_size]. 
+            # sequence_length * input_size must be equal to 784
             with tf.name_scope('input') as scope:
-                self.x = tf.placeholder(tf.float32, shape=[None, sequence_length*input_size], name='x-input')
+                self.x = tf.placeholder(tf.float32, shape=[None, sequence_length * input_size], name='x-input')
                 with tf.name_scope('input-preprocess'):
-                    # Get x input in correct format [batch_size, ]
+
+                    # Get x input in correct format
                     x_squared = tf.reshape(self.x, [-1, sequence_length, input_size])
                     x_transposed = tf.transpose(x_squared, [1,0,2])
                     x_reshaped = tf.reshape(x_transposed, [-1, input_size])
@@ -21,36 +23,25 @@ class RecurrentNeuralNetwork(object):
                 # Target input with shape [?, 10]
                 with tf.name_scope('target_input') as scope:
                     self.y_ = tf.placeholder(tf.float32, shape=[None, 10], name='target-output')
-
-            # Hidden layer 1
-           
-                # Hidden layer 1 weights and bias
                 
             lstm_cell = tf.contrib.rnn.BasicLSTMCell(hidden_size, forget_bias=1.0, state_is_tuple=True)
-            #state = tf.zeros([batch_size, lstm_cell.state_size])
             with tf.name_scope('initial_state') as scope:
                 state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
 
 
-            #state = lstm_cell.zero_state(batch_size, tf.float32)
             outputs, state = tf.contrib.rnn.static_rnn(lstm_cell, x_split, state)
-            #print 'outputs.shape: {}'.format(outputs.get_shape())
-            #print 'states.shape: {}'.format(states.get_shape())
+            
             # Output with shape [?, 10]
             with tf.name_scope('output') as scope:
                 W = tf.Variable(tf.truncated_normal([hidden_size, 10]), name='weights-1')
                 b = tf.Variable(tf.zeros([10]), name='bias-1')
                 self.y = tf.matmul(outputs[-1], W) + b
-
-            # Objective function - Cross Entropy
-            # E = - SUM(y_ * log(y))
             
             with tf.name_scope('optimizer') as scope:
 
                 with tf.name_scope('loss') as scope:
+                    # Objective function - Cross Entropy
                     self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.y, labels=self.y_))
-                    #self.obj_function = tf.reduce_mean(tf.reduce_sum(self.y_ * tf.log(self.y), reduction_indices=[1]))
-                    #
                 
                 if optimizer.lower() == 'adam':
                     # Adam Optimizer
@@ -61,7 +52,6 @@ class RecurrentNeuralNetwork(object):
                 else: 
                     # Gradient Descent
                     self.train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(self.loss)
-
 
             # Specify how accuracy is measured
             with tf.name_scope('accuracy') as scope:
